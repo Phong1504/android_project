eca0b48f52724b0976a11bfd1fdf7d1292debc5e
anhthv2thea <anhthv.thea2@gmail.com>
2023-11-09 00:57:34 +0700

    Login/Register form (stage 4 - Change BottomNavigation to TabLayout + ViewPager )  from anhthv.thea2

--- app/src/main/java/com/example/mucsicapp/Activity/MainActivity.java
+++ app/src/main/java/com/example/mucsicapp/Activity/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.mucsicapp.Activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.mucsicapp.Adapter.MainViewPagerAdapter;
 import com.example.mucsicapp.Fragment.tab_ca_nhan;
 import com.example.mucsicapp.Fragment.tab_tim_kiem;
 import com.example.mucsicapp.Fragment.tab_trang_chu;
@@ -14,37 +16,33 @@ import com.example.mucsicapp.R;
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     tab_trang_chu trangchuFragment = new tab_trang_chu();
     tab_tim_kiem timkiemFragment = new tab_tim_kiem();
     tab_ca_nhan canhanFragment = new tab_ca_nhan();
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, trangchuFragment).commit();
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if(item.getItemId() == R.id.trang_chu) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, trangchuFragment).commit();
-                    Intent intent = new Intent(MainActivity.this, trangchuFragment.getClass());
-                    return true;
-                }else if(item.getItemId() == R.id.tim_kiem) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, timkiemFragment).commit();
-                    Intent intent = new Intent(MainActivity.this, timkiemFragment.getClass());
-                    return true;
-                }else if(item.getItemId() == R.id.ca_nhan) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, canhanFragment).commit();
-                    Intent intent = new Intent(MainActivity.this, canhanFragment.getClass());
-                    return true;
-                }else
-                    return false;
-            };
-        });
+        //Thao tac vuot
+        tabLayout = findViewById(R.id.myTabLayout);
+        viewPager = findViewById(R.id.myViewPager);
+        MainViewPagerAdapter mainViewPagerAdapter = new MainViewPagerAdapter(getSupportFragmentManager());
+        mainViewPagerAdapter.addFragment(new tab_trang_chu(), "Trang Chủ");
+        mainViewPagerAdapter.addFragment(new tab_tim_kiem(), "Tìm Kiếm");
+        mainViewPagerAdapter.addFragment(new tab_ca_nhan(), "Cá Nhân");
+        viewPager.setAdapter(mainViewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.icontrangchu);
+        tabLayout.getTabAt(1).setIcon(R.drawable.icontimkiem);
+        tabLayout.getTabAt(2).setIcon(R.drawable.iconcanhan);
+
     }
 }
