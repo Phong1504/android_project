073d3bb0c5a4c082d36903de2aa17451a2675479
bhuonguit <134910682+bhuonguit@users.noreply.github.com>
2023-12-30 17:22:47 +0700

    80%-con 2man hinh tim kiem va ca nhan
    Phan DO danh sach bai hat qua man hinh play nhac khong hien thi , can fix

---
+++ 80%-Huong/app/src/main/java/API/DataService.java
@@ -0,0 +1,39 @@
+package API;
+
+import com.example.mucsicapp.Model.Album;
+import com.example.mucsicapp.Model.Baihat;
+import com.example.mucsicapp.Model.QuangCao;
+import com.example.mucsicapp.Model.TheLoai;
+import com.example.mucsicapp.Model.Theloaitrongngay;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface DataService {
+    @GET("example22.php")
+    Call<List<QuangCao>> GetDataBanner();
+    @GET("chudetheloai.php")
+    Call<Theloaitrongngay> GetCategoryMusic();
+
+    @GET ("Album.php")
+    Call<List<Album>> GetAlbum();
+    @GET("chudetheloai.php")
+    Call<List<TheLoai>> GetChude();
+
+    @GET ("baihhatyeuthich.php")
+    Call<List<Baihat>> GetBaihatHot();
+
+    @FormUrlEncoded
+    @POST("danhsachbaihat.php")
+    Call<List<Baihat>> GetDsbaihatquangcao(@Field("idquangcao") String idquangcao);
+
+    @FormUrlEncoded
+    @POST("danhsachbaihat.php")
+    Call<List<Baihat>> GetDanhsachtheotheloai(@Field("idtheloai") String idtheloai);
+
+}
