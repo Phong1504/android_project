f13cc6bbdb0bd59e99e0dc31b295e69d5d012ed2
bhuonguit <134910682+bhuonguit@users.noreply.github.com>
2023-12-31 14:17:01 +0700

    can them tim kiem va ca nhan
    sap xong

---
+++ 31.12/ganhoanthien/app/src/main/java/com/example/mucsicapp/Activity/DangNhap_DangKy.java
@@ -0,0 +1,48 @@
+package com.example.mucsicapp.Activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mucsicapp.R;
+
+public class DangNhap_DangKy extends AppCompatActivity {
+    private Button btnDangKy, btnDangNhap;
+    @Override
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.man_hinh_dau_tien);
+
+        btnDangNhap = findViewById(R.id.btn_dangnhap);
+        btnDangKy = findViewById(R.id.btn_dangky);
+
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DangNhapVaoApp();
+            }
+        });
+        btnDangKy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DangKyTaiKhoan();
+            }
+        });
+    }
+
+    private void DangNhapVaoApp(){
+            Intent iDangNhap = new Intent(DangNhap_DangKy.this, DangNhapActivity.class);
+            startActivity(iDangNhap);
+    }
+    private void DangKyTaiKhoan(){
+        Intent iDangKy = new Intent(DangNhap_DangKy.this, DangKyActivity.class);
+        startActivity(iDangKy);
+    }
+
+}
---
+++ 31.12/ganhoanthien/app/src/main/java/com/example/mucsicapp/Adapter/BannerAdapter.java
@@ -0,0 +1,75 @@
+package com.example.mucsicapp.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.mucsicapp.Activity.DanhsachBaihatActivity;
+import com.example.mucsicapp.Model.QuangCao;
+import com.example.mucsicapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class BannerAdapter extends PagerAdapter {
+    Context context;
+    ArrayList<QuangCao> arrayListBanner;
+    public BannerAdapter(Context context, ArrayList<QuangCao> arrayListBanner){
+        this.context = context;
+        this.arrayListBanner = arrayListBanner;
+
+    }
+
+    @Override
+    public int getCount() {
+        return arrayListBanner.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.dong_banner, null);
+
+        ImageView imagebackroundbanner = view.findViewById(R.id.imageviewbackgroungbanner);
+        ImageView imagebanner = view.findViewById(R.id.imageview_banner);
+        TextView titlebanner = view.findViewById(R.id.Textviewtitlebannerbaihat);
+        TextView textviewmota = view.findViewById(R.id.textviewmota);
+
+        Picasso.get().load(arrayListBanner.get(position).getHinhanhQuangcao()).into(imagebackroundbanner);
+        Picasso.get().load(arrayListBanner.get(position).getHinhBaihat()).into(imagebanner);
+        textviewmota.setText(arrayListBanner.get(position).getNoiDung());
+        titlebanner.setText(arrayListBanner.get(position).getTenBaiHat());
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(context, "Lolo", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, DanhsachBaihatActivity.class);
+                intent.putExtra("banner", arrayListBanner.get(position));
+                context.startActivity(intent);
+            }
+        });
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
